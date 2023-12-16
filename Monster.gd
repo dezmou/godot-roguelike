3ddadc210a7bff0e51616509ae99c2b737c6e9ee
= <incredibledez@gmail.com>
2023-12-16 12:44:43 +0100

    wip

---
+++ Monster.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+enum Player {you, bot}
+enum Type {soldier}
+
+func init(player : Player, type : Type):
+	pass
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
