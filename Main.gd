92bb6222494d521ffc62e16fc9f1918b791be667
= <incredibledez@gmail.com>
2023-12-16 14:14:09 +0100

    wip

--- Main.gd
+++ Main.gd
@@ -1,4 +1,5 @@
 extends Node2D
+enum Player {YOU, BOT}
 
 const Knife = preload("res://monsters/knife/knife.tscn")
 
@@ -6,8 +7,9 @@ const Knife = preload("res://monsters/knife/knife.tscn")
 func _ready():
 	for i in range(2):
 		var knife = Knife.instantiate()
-		knife.position.x = 150 * i
-		knife.position.y = 300 * i
+		knife.init(Player.YOU if i == 0 else Player.BOT)
+		knife.position.x = 150 * (i + 1)
+		knife.position.y = 300 * (i + 1)
 		add_child(knife)
 
 
--- monsters/monster.gd
+++ monsters/monster.gd
@@ -1,8 +1,14 @@
 extends RigidBody2D
 
-enum Player {You, Bot}
+const Main = preload("res://Main.gd")
+
 var health = 10
 var type = "base"
+var player : Main.Player
+
+func init(player : Main.Player):
+	self.player = player
+	$playerColor.color = Color(0.7,0.2,0.1,0.5) if player == Main.Player.BOT else  Color(0.2,0.7,0.1,0.5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
